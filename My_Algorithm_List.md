9c1704bd2c435f9b22f2d1cc69e6ac7c675b064c
limgeonho <ghlim909@gmail.com>
2021-08-15 02:53:08 +0900

    2021.08.15

--- My_Algorithm_List.md
+++ My_Algorithm_List.md
@@ -4,6 +4,8 @@
 
 - disjoint_set - 서로소 집합
 - kruskal - 크루스칼
+- prim - 프림
+- topology_sort - 위상정렬(그래프)
 
 ## 2. 다이나믹 프로그래밍
 
@@ -17,8 +19,8 @@
 ## 4. 정렬 알고리즘
 
 - bubble_sort - 버블정렬
-- counting_sort - 카운팅 정렬(계수정렬)
-- topology_sort - 위상정렬(그래프)
+- counting_sort - 카운팅정렬(계수정렬)
+- selection_sort - 선택정렬
 
 ## 5. 최단경로 알고리즘
 
@@ -34,4 +36,11 @@
 - permutation
 - combination
 - product
-- prime_number
+- prime_number1, 2
+- subset
+- two_pointer
+
+## 7. 행렬 알고리즘
+
+- multiply_matrix
+- rotate_matrix
--- 그래프_알고리즘/prim.py
+++ 그래프_알고리즘/prim.py
@@ -1,1 +1,46 @@
-# BOJ 20927
+# 프림 알고리즘(크루스칼과 비슷)
+# 크루스칼 알고리즘과 같은 목적으로 MST를 확인하는데 주 목적이 있다.
+# 크루스칼은 간선의 가중치를 오름차순으로 정렬한 후 선택하는 반면에
+# 프림알고리즘은 한 정점을 선택하고 이어지는 정점들 중 간선이 가장 작은 것을 선택해 나간다.
+# 간선이 많은 경우에는 크루스칼보다 프림이 더 유리하다
+# 구현하는 과정은 다익스트라와 비슷함
+
+from collections import defaultdict
+import heapq
+import sys
+input = sys.stdin.readline
+
+
+n, m = map(int, input().split())    # n은 노드 수, m은 간선 수
+graph = defaultdict(list)   # 빈 그래프 생성
+visited = [0] * (n+1)   # 노드 방문 확인 리스트
+
+# 무방향 그래프
+for i in range(m):
+    a, b, cost = map(int, input().split())
+    graph[a].append([cost, a, b])
+    graph[b].append([cost, b, a])
+
+
+# prim_algorithm
+def prim(graph, start):
+    visited[start] = 1    # 첫 노드 방문 체크 => 1
+    candidate = graph[start]    # 인접 간선 정보 추출
+    heapq.heapify(candidate)    # heapq 생성
+    mst = []    # 최소 신장 트리
+    result = 0    # 전체 가중치
+
+    while candidate:
+        cost, a, b = heapq.heappop(candidate)   # 가중치가 가장 적은 간선 정보 추출
+        if visited(b) == 0:   # 방문하지 않았다면
+            visited[b] = 1    # 방문 갱신
+            mst.append((a, b))    # mst에 추가
+            result += cost    # 가중치 누적
+
+            for edge in graph[b]:   # 다음 인접 간선 탐색
+                if visited[edge[2]] == 0:   # 방문한 노드가 아니라면
+                    heapq.heappush(candidate, edge)
+    return result
+
+
+print(prim(graph, 1))
