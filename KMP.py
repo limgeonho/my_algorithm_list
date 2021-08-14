6f32829a25450c05c0a73445a2701bb770e12301
limgeonho <ghlim909@gmail.com>
2021-08-14 22:27:55 +0900

    2021.08.14

---
+++ KMP.py
@@ -0,0 +1,24 @@
+# KMP 알고리즘(패턴을 찾는 알고리즘)
+
+# 일반적인 문자열 찾기 알고리즘
+
+def find_string(parent, pattern):
+
+    parent_length = len(parent)
+    pattern_length = len(pattern)
+
+    for i in range(parent_length - pattern_length + 1):
+        finded = True
+        for j in range(pattern_length):
+            if parent[i + j] != pattern[j]:
+                finded = False
+                break
+        if finded:
+            return i
+    return -1
+
+
+print(find_string('hello world', 'llo'))
+
+
+# =======================================================================
