201032d448f4dd073d161a091b608217ea580a0b
limgeonho <ghlim909@gmail.com>
2021-08-16 23:35:03 +0900

    2021.08.16

--- 이진탐색_알고리즘/binary_search_2.py
+++ 이진탐색_알고리즘/binary_search_2.py
@@ -1,12 +1,9 @@
+from bisect import bisect_left, bisect_right
+# 이진탐색 라이브러리(bisect)활용하기
 # from bisect import bisect_left, bisect_right
-# # 이진탐색 라이브러리(bisect)활용하기
-# # from bisect import bisect_left, bisect_right
+# bisect_left(array, x) => array안에 x가 삽입되어야할 위치를 반환하고 이미 x가 존재할 경우 x의 왼쪽 위치를 반환한다.
+array = [1, 3, 5, 7, 9]
+x = 4
 
-# array = [1, 2, 3, 5, 6]
-# x = 3
-
-# print(bisect_left(array, x))
+print(bisect_left(array, x))
 # print(bisect_right(array, x))
-
-
-# 추가적으로 공부 필요!!
