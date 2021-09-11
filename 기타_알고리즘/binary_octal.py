0c8177cec48d02cec27b5766fec4a5ce52c01bff
limgeonho <ghlim909@gmail.com>
2021-09-11 20:43:27 +0900

    2021.09.11

---
+++ 기타_알고리즘/binary_octal.py
@@ -0,0 +1,21 @@
+# 진수 변환 방법
+
+# n진수 -> 10 진수
+# int(<해당 진수로 만들어진 수>, 해당진수) -> 10진수 값으로 변환된다.
+print(int('111', 2))
+
+# 10진수 -> n진수
+
+
+def solution(n, q):
+    rev_base = ''
+
+    while n > 0:
+        n, mod = divmod(n, q)
+        rev_base += str(mod)
+
+    return rev_base[::-1]
+    # 역순인 진수를 뒤집어 줘야 원래 변환 하고자하는 base가 출력
+
+
+print(solution(45, 3))
