f3feaae9c4a72f9b921cf4748a312ac8e6126f9c
limgeonho <ghlim909@gmail.com>
2021-09-19 02:51:01 +0900

    2021.09.19

--- 기타_알고리즘/n_queen.py
+++ 기타_알고리즘/n_queen.py
@@ -5,29 +5,34 @@
 
 # ===========================================================================================
 def is_available(candidate, current_col):
-    current_row = len(candidate)
+    current_row = len(candidate)  # 선택된 queen list의 길이, 개수 = 현재 행의 위치
     for queen_row in range(current_row):
+        # 세로줄 탐색 or 대각선 탐색(2가지임) => 이 조건문이 핵심!
         if candidate[queen_row] == current_col or abs(candidate[queen_row] - current_col) == current_row - queen_row:
             return False
     return True
 
 
+# current_row : 현재 행 / current_candidate : 현재까지 선택된 queen
 def DFS(N, current_row, current_candidate, final_result):
     if current_row == N:
         final_result.append(current_candidate[:])
         return
 
+    # current_row에서 한 칸씩 오른쪽으로 이동하며 queen을 놓을 자리를 선택
     for candidate_col in range(N):
-        if is_available(current_candidate, candidate_col):
-            current_candidate.append(candidate_col)
-            DFS(N, current_row + 1, current_candidate, final_result)
-            current_candidate.pop()
+        if is_available(current_candidate, candidate_col):  # 대각선, 세로 여부 파악
+            current_candidate.append(candidate_col)  # 하나의 queen을 선택하고 후보에 넣음
+            DFS(N, current_row + 1, current_candidate, final_result)  # 다음줄로 넘어감
+            current_candidate.pop()  # 아직 남은 후보를 확인하기 위해 마지막 후보를 pop하고 나머지 순회
 
 
-def solve_n_queens(N):
-    final_result = []
+def solve_n_queens(N):  # 체스판 크기입력(n*n)
+    final_result = []   # n-queen이 가능한 queen들의 위치
     DFS(N, 0, [], final_result)
-    return final_result
+    print(len(final_result))
+    return
+
 
 # ===========================================================================================
 
