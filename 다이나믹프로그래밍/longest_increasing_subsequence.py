ed17edef2e8534f38622c3f7e43f0fd0b0a6a990
limgeonho <ghlim909@gmail.com>
2021-09-29 02:54:00 +0900

    2021.09.29

--- 다이나믹프로그래밍/longest_increasing_subsequence.py
+++ 다이나믹프로그래밍/longest_increasing_subsequence.py
@@ -61,3 +61,43 @@ for i in range(n-1, -1, -1):
         order -= 1
 
 print(*answer[::-1])
+
+
+# ======================================================================
+# 4. 수열의 길이가 길고 형태까지 출력해야하는 경우
+# 수열의 길이가 길다면 O(n)으로 해결해야 하기 때문에 => bisect_left를 이용
+# LIS의 형태를 출력해야 하기 때문에 기존의 역추적방법을 이용 => LIS의 길이를 저장할 dp리스트를 추가해서 이용
+# 따라서 위의 2번과 3번의 방법을 합친 것임
+
+
+n = int(input())
+array = [0] + list(map(int, input().split()))
+# bisect_left를 이용해서 LIS길이만 사용하기 위해 이용
+tmp = [-1000000001]
+# i번째가 가장 마지막 수 일때 LIS길이를 저장
+dp = [0] * (n + 1)
+max_val = 0
+
+for i in range(1, n + 1):
+    if tmp[-1] < array[i]:
+        tmp.append(array[i])
+        dp[i] = len(tmp) - 1
+
+    else:
+        dp[i] = bisect_left(tmp, array[i])
+        tmp[dp[i]] = array[i]
+
+# LIS의 길이 == max_val
+max_val = max(dp)
+answer = []
+
+for i in range(n, 0, -1):
+    # max_val == order의 시작점 거꾸로 돌것임
+    if dp[i] == max_val:
+        answer.append(array[i])
+        max_val -= 1
+
+print(max_val)
+print(dp)
+print(tmp)
+print(*answer[::-1])
