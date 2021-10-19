f79ebe3a71a81b53e2c0bdd1bf9dfaac8f448713
limgeonho <ghlim909@gmail.com>
2021-10-20 03:00:24 +0900

    2021.10.20

--- basic_of_basic/basic_things.py
+++ basic_of_basic/basic_things.py
@@ -1,93 +1,64 @@
-# 기본중의 기본 순열, 조합, 중복순열
+# 기본중의 기본 순열, 조합, 중복순열 / 부분집합 => 재귀문제 거의 해결
 
-N, M = map(int, input().split())
+n = int(input())
+array = list(map(int, input().split()))
 
-path = [0 for _ in range(N)]
-used = [0 for _ in range(7)]
-
-
-# 중복순열
-def run1(lev):
-    if lev == N:
-        print(*path)
-        return
-    for i in range(1, 7):
-        path[lev] = i
-        run1(lev+1)
+visited = [False] * len(array)
+res = [0] * n
 
 
-# 조합
-def run2(lev, start):
-    if lev == N:
-        print(*path)
-        return
-    for i in range(start, 7):
-        path[lev] = i
-        run2(lev+1, i)
-
+# ====================================
 # 순열
-
-
-def run3(lev):
-    if lev == N:
-        print(*path)
+def perm(L):
+    if L == n:
+        print(*res)
         return
-    for i in range(1, 7):
-        if used[i] == 1:
+    for i in range(len(array)):
+        if visited[i]:
             continue
-        used[i] = 1
-        path[lev] = i
-        run3(lev + 1)
-        used[i] = 0
+        visited[i] = True
+        res[L] = array[i]
+        perm(L+1)
+        visited[i] = False
+
+# perm(0)
 
 
-if M == 1:
-    run1(0)
-elif M == 2:
-    run2(0, 1)
-elif M == 3:
-    run3(0)
+# ====================================
+# 중복순열
+def perm_with(L):
+    if L == n:
+        print(*res)
+        return
+    for i in range(len(array)):
+        res[L] = array[i]
+        perm_with(L+1)
+
+# perm_with(0)
 
 
-# ========================================================================
-# def type1(t):
-#     if t == n:
-#         print(*arr)
-#         return
-#     for i in range(1, 7):
-#         arr.append(i)
-#         type1(t+1)
-#         arr.pop()
+# ====================================
+# 조합
+def comb(L, start):
+    if L == n:
+        print(*res)
+        return
+    for i in range(start, len(array)):
+        res[L] = array[i]
+        comb(L+1, i+1)
 
-# def type2(t, num):
-#     if t == n:
-#         print(*arr)
-#         return
-#     for i in range(num, 7):
-#         arr.append(i)
-#         type2(t+1, i)
-#         arr.pop()
+# comb(0, 0)
 
-# def type3(t):
-#     if t == n:
-#         print(*arr)
-#         return
-#     for i in range(1, 7):
-#         if used[i]:
-#             continue
-#         arr.append(i)
-#         used[i] = 1
-#         type3(t+1)
-#         arr.pop()
-#         used[i] = 0
 
-# n, m = map(int, input().split())
+# ====================================
+# 부분집합
+def powerset(L, ps):
+    if L == n:
+        if not ps:                  # 공집합은 제거
+            return
+        print(ps)
+        return
+    powerset(L+1, ps + [array[L]])  # 해당 원소를 선택 O
+    powerset(L+1, ps)               # 해당 원소를 선택 X
 
-# arr = []
-# used = [0] * 7
-# if m == 1:
-#     type1(0)
-# elif m == 2:
-#     type2(0, 1)
-# elif m == 3:
-#     type3(0)
+#powerset(0, [])
